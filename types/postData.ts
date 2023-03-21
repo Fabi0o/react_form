3da0ba4a7da8fada44cc51b7b4e563ccf483c8a3
Fabi0o <marfab32@gmail.com>
2023-03-21 11:56:40 +0100

    add postData type

---
+++ types/postData.ts
@@ -0,0 +1,11 @@
+import Dishes from "./dishes";
+
+export default interface PostData {
+  name: string;
+  preparation_time: string;
+  type: Dishes;
+  no_of_slices?: number;
+  diameter?: number;
+  spiciness_scale?: number;
+  slices_of_breads?: number;
+}
